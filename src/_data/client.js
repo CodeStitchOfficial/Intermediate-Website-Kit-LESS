db3a0d9cc5d2f887c6e472602599b1da0eaa3f5e
jaylamorjaria <jay@veryclearthinking.com>
2024-08-02 12:08:47 +0100

    commit

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,20 +1,18 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "JD Wills and Estates",
+    "email": "hello@jdwillsandestates.co.uk",
+    "phoneForTel": "+447983259577",
+    "phoneFormatted": "07983 259577",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
+        "lineOne": "414 Welford Road",
+        "city": "Leicester",
+        "state": "Leicestershire",
+        "zip": "LE2 6EJ",
+        "mapLink": "https://www.google.com/maps/place/212+Welford+Rd,+Leicester+LE2+6BD/@52.6153765,-1.1249712,17z/data=!3m1!4b1!4m6!3m5!1s0x4877612dbb67deef:0x788f1ce2f8670f57!8m2!3d52.6153765!4d-1.1249712!16s%2Fg%2F11vx5l3blq?entry=ttu"
     },
     "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
+        "Linkedin": "https://www.linkedin.com/company/jd-wills-and-estates/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://www.jdwillsandestates.co.uk"
 };
